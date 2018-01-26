b0b54c339d46db27475cc637d11e273173b8b006
ganemcos <ganemcos@gmail.com>
2018-01-26 10:35:12 +0530

    Initial commit

---
+++ app/src/main/java/com/sportsme/saiganesh/sportsme/Help.java
@@ -0,0 +1,25 @@
+package com.sportsme.saiganesh.sportsme;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Acer on 24-01-2018.
+ */
+
+public class Help extends Fragment{
+
+    View myview;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        myview = inflater.inflate(R.layout.help,container,false);
+        return myview;
+    }
+}
+
